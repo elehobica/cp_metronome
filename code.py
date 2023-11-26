2ebff491ef54fadf3c674fda0f0448c76227f630
elehobica <elehobica@gmail.com>
2023-11-26 23:01:14 +0900

    support beat selection from 1 to 10

--- code.py
+++ code.py
@@ -9,9 +9,11 @@ piezo = pwmio.PWMOut(board.A2, duty_cycle=0, frequency=440, variable_frequency=T
 freq = 1397
 
 count = 0
-bpm = 86  # Moderato
 inc = 1
-level_show = 0
+bpm = 86  # Moderato
+bpm_show = 0
+beat = 4
+beat_show = 0
 
 while True:
     # Start time
@@ -20,13 +22,42 @@ while True:
     # Get button status
     bt_a = cp.button_a
     bt_b = cp.button_b and not bt_a
-    if not bt_a and not bt_b:
-        inc = 1
-        if level_show > 0:
-            level_show -= 1
+    if cp.switch:
+        beat_show = 0
+        if not bt_a and not bt_b:
+            inc = 1
+            if bpm_show > 0:
+                bpm_show -= 1
+        else:
+            bpm_show = 3
+        if bt_a:
+            if bpm - inc > 30:
+                bpm -= inc
+                inc += 1
+            else:
+                bpm = 30
+        if bt_b:
+            if bpm + inc < 252:
+                bpm += inc
+                inc += 1
+            else:
+                bpm = 252
     else:
-        level_show = 3
-    if level_show > 0:
+        bpm_show = 0
+        if not bt_a and not bt_b:
+            if beat_show > 0:
+                beat_show -= 1
+        else:
+            beat_show = 3
+        if bt_a:
+            if beat > 1:
+                beat -= 1
+        if bt_b:
+            if beat < 10:
+                beat += 1
+
+    # bpm level
+    if bpm_show > 0:
         if bpm < 40:
             level = 0
         elif bpm < 60:
@@ -55,36 +86,30 @@ while True:
         for i in range(level, 10):
             j = 4 - (i % 5) + (i // 5) * 5 # reorder 4, 3, 2, 1, 0, 9, 8, 7, 6, 5
             cp.pixels[j] = (0, 0, 0)
-    # Light & Sound
+    elif beat_show > 0:
+        for i in range(beat):
+            j = 4 - (i % 5) + (i // 5) * 5 # reorder 4, 3, 2, 1, 0, 9, 8, 7, 6, 5
+            cp.pixels[j] = (50, 50, 0)
+        for i in range(beat, 10):
+            j = 4 - (i % 5) + (i // 5) * 5 # reorder 4, 3, 2, 1, 0, 9, 8, 7, 6, 5
+            cp.pixels[j] = (0, 0, 0)
+
+    # Metronome Light & Sound
     piezo.duty_cycle = 65535 // 2  # On 50%
-    if count % 4 == 0:
-        if level_show == 0:
+    if count % beat == 0:
+        if bpm_show == 0 and beat_show == 0:
             cp.pixels.fill((0, 50, 0))
         piezo.frequency = freq * 2
     else:
-        if level_show == 0:
+        if bpm_show == 0 and beat_show == 0:
             cp.pixels.fill((50, 0, 0))
         piezo.frequency = freq
     time.sleep(0.05)
-    if level_show == 0:
+    if bpm_show == 0 and beat_show == 0:
         cp.pixels.fill((0, 0, 0))
     time.sleep(0.05)
     piezo.duty_cycle = 0  # OFF
 
-    # Button I/F
-    if bt_a:
-        if bpm - inc > 30:
-            bpm -= inc
-            inc += 1
-        else:
-            bpm = 30
-    if bt_b:
-        if bpm + inc < 252:
-            bpm += inc
-            inc += 1
-        else:
-            bpm = 252
     count += 1
-
     # Set interval depending on bpm
     time.sleep(60 / bpm - (time.monotonic() - t))
